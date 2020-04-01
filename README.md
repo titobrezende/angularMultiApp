06322c438dfb678ca27266178e8ee0813aabd0d0
titobr <titobr@motorola.com>
2020-04-01 14:46:09 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is just a test of combining Multiple Angular applications into a single one
+
+Following the tutorial on this link:
+
+https://medium.com/disney-streaming/combining-multiple-angular-applications-into-a-single-one-e87d530d6527
---
+++ projects/app2/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
+import { NavComponent } from './nav/nav.component';
+
+const appProviders = [];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    View1Component,
+    View2Component,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: appProviders,
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+@NgModule({})
+export class App2SharedModule {
+  static forRoot(): ModuleWithProviders<AppModule> {
+    return {
+      ngModule: AppModule,
+      providers: appProviders
+    };
+  }
+}
